1fba2f50e0fc4c86915dea871da418acb159d6ce
awsdang <awsdang@gmail.com>
2025-03-17 13:46:02 +0300

    header and first ui implementations

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,19 @@
 
+import Header from "@/components/header"
+
 function App() {
 
   return (
     <>
-    <div className='bg-black text-white'>hello world</div>
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+     
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
