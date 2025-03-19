db913a8a8e98a7544b7c56a4d7e92c8c9a026731
awsdang <awsdang@gmail.com>
2025-03-19 14:49:44 +0300

    update refresh interval

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -24,7 +24,7 @@ interface StoreState {
 }
 
 export const useStore = create<StoreState>((set) => ({
-    refreshInterval: 3000,
+    refreshInterval: 2000,
     connectionStatus: "connecting",
     timeRange: "week",
     filters: {
